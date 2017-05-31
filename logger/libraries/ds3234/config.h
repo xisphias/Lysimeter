2440a497769f4dc549f762f5a54a06ed5dbe1336
xisphias <xisphias@gmail.com>
2017-05-31 15:23:34 -0800

    therm library

--- logger/libraries/ds3234/config.h
+++ logger/libraries/ds3234/config.h
@@ -1,7 +1,7 @@
 #ifndef __config_h_
 #define __config_h_
 
-// comment this out if you need unixtime support
+// comment this out if you DONT need unixtime support
 // this will add about 380bytes to your firmware
 #define CONFIG_UNIXTIME
 
